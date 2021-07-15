b45361b1a4ad522144452716b1c46949feeb73fa
i-cm <74533816+i-cm@users.noreply.github.com>
2021-07-15 18:59:28 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,26 @@
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    game.over(false)
-})
-let mySprite2: Sprite = null
-let mySprite = sprites.create(img`
-    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-    3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
-    3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
-    3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
-    3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
-    3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
-    3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
-    3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
-    3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
-    3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
-    3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
-    3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
-    3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
-    3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
-    3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
-    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-    `, SpriteKind.Player)
-scene.setBackgroundColor(7)
-controller.moveSprite(mySprite)
-game.onUpdateInterval(2000, function () {
+function Start_game () {
+    mySprite = sprites.create(img`
+        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+        3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+        3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+        3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+        3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+        3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+        3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+        3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+        3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+        3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+        3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+        3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+        3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+        3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+        3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+        3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+        `, SpriteKind.Player)
+    scene.setBackgroundColor(7)
+    controller.moveSprite(mySprite)
+}
+function Enemys (x: number, y: number) {
     mySprite2 = sprites.create(img`
         2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
         2 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 
@@ -41,7 +39,16 @@ game.onUpdateInterval(2000, function () {
         2 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 
         2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
         `, SpriteKind.Enemy)
-    mySprite2.setPosition(randint(0, 160), randint(0, 160))
+    mySprite2.setPosition(x, y)
     info.changeScoreBy(1)
     music.baDing.play()
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    game.over(false)
+})
+let mySprite2: Sprite = null
+let mySprite: Sprite = null
+Start_game()
+game.onUpdateInterval(2000, function () {
+    Enemys(randint(0, 160), randint(0, 160))
 })
